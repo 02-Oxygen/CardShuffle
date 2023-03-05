98dc1c997fb1870c0006e39b450a207406603fdb
02-Oxygen <124358098+02-Oxygen@users.noreply.github.com>
2023-03-05 12:07:12 +0000

    Add files via upload

---
+++ CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Pack
+    {
+        List<Card> pack;
+
+        public Pack()
+        {
+            //Initialise the card pack here
+        }
+
+        public static bool shuffleCardPack(int typeOfShuffle)
+        {
+            //Shuffles the pack based on the type of shuffle
+
+        }
+        public static Card deal()
+        {
+            //Deals one card
+
+        }
+        public static List<Card> dealCard(int amount)
+        {
+            //Deals the number of cards specified by 'amount'
+        }
+
+        public static void createPack()
+        {
+            List<Card> temp = new List<Card>();
+            for (int i = 0; i < 4; i++)
+            {
+
+            }
+        }
+    }
+}
